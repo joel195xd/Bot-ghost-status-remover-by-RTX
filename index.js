fdda3f32b42b9cf55c93b171cc9f4766d9774468
Dark <78479220+joel195xd@users.noreply.github.com>
2024-03-23 16:56:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Urban Roleplay","Bot oficial"];
 
 
 let currentIndex = 0;
